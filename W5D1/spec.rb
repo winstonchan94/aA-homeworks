bf9fa17d7d94e01d8680c67b5d171cba810d1919
Winston Chan <winston.chan94@gmail.com>
2018-03-11 21:03:14 -0700

    Do hw

---
+++ W5D1/spec.rb
@@ -0,0 +1,84 @@
+
+require 'rails_helper'
+
+describe User do
+  subject(:user) do
+    FactoryBot.build(:user,
+      email: "jonathan@fakesite.com",
+      password: "good_password")
+  end
+
+  it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:password_digest) }
+  it { should validate_length_of(:password).is_at_least(6) }
+
+  it "creates a password digest when a password is given" do
+    expect(user.password_digest).to_not be_nil
+  end
+
+  it "creates a session token before validation" do
+    user.valid?
+    expect(user.session_token).to_not be_nil
+  end
+
+  describe "#reset_session_token!" do
+    it "sets a new session token on the user" do
+      user.valid?
+      old_session_token = user.session_token
+      user.reset_session_token!
+
+      expect(user.session_token).to_not eq(old_session_token)
+    end
+
+    it "returns the new session token" do
+      expect(user.reset_session_token!).to eq(user.session_token)
+    end
+  end
+
+  describe "#is_password?" do
+    it "verifies a password is correct" do
+      expect(user.is_password?("good_password")).to be true
+    end
+
+    it "verifies a password is not correct" do
+      expect(user.is_password?("bad_password")).to be false
+    end
+  end
+
+
+end
+require 'rails_helper'
+
+feature "the signup process" do
+
+  scenario "has a new user page" do
+    visit new_user_url
+    expect(page).to have_content "New User"
+  end
+
+  feature "signing up a user" do
+    before(:each) do
+      visit new_user_url
+      fill_in 'Email', :with => "testing@email.com"
+      fill_in 'Password', :with => "biscuits"
+      click_on "create user"
+    end
+
+    scenario "redirects to sign-in page after signup" do
+      expect(page).to have_content "Successfully created your account! Check your inbox for an activation email."
+    end
+  end
+
+  feature "with an invalid user" do
+    before(:each) do
+      visit new_user_url
+      fill_in 'Email', :with => "testing@email.com"
+      click_on "create user"
+    end
+
+    scenario "re-renders the new user page after failed signup" do
+      expect(page).to have_content "Password is too short (minimum is 6 characters)"
+    end
+  end
+
+end
